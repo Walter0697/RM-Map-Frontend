5ec074c314f9d9a5ef13db2838172cad033872e7
Walter Cheng <walterpakwai@live.com>
2022-08-05 00:38:59 +0800

    implemented download image

--- src/components/marker/MarkerShareView.js
+++ src/components/marker/MarkerShareView.js
@@ -12,6 +12,8 @@ import {
 import SaveIcon from '@mui/icons-material/Save'
 import SendIcon from '@mui/icons-material/Send'
 
+import CircleIconButton from '../field/CircleIconButton'
+
 import generate from '../../scripts/generate'
 
 const TransitionUp = (props) => {
@@ -24,14 +26,25 @@ function MarkerShareView({
     marker,
     eventtypes,
 }) {
+    const [ generating, setGenerating ] = useState(false)
     const [ previewImage, setPreviewImage ] = useState(null)
 
     const setImage = async () => {
-
+        setGenerating(true)
         const eventtype = eventtypes.find(s => s.value === marker.type)
         const generated = await generate.preview.generatePreviewImage(marker, eventtype)
         
         setPreviewImage(generated)
+        setGenerating(false)
+    }
+
+    const downloadImage = async () => {
+        var a = document.createElement('a')
+        a.href = previewImage
+        a.download = `${marker.label}.png`
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
     }
 
     useEffect(() => {
@@ -57,19 +70,54 @@ function MarkerShareView({
                         </DialogTitle>
                         <DialogContent dividers>
                             <DialogContentText>
-                            <div style={{
-                                display: 'flex',
-                                justifyContent: 'center'
-                            }}>
-                                {previewImage && (
-                                    <img 
-                                        style={{
-                                            width: '90%',
-                                        }} 
-                                        src={previewImage} 
-                                    />
-                                )}
-                            </div>
+                                <Grid container>
+                                    <Grid item xs={12} md={12} lg={12}>
+                                        <div style={{
+                                            display: 'flex',
+                                            justifyContent: 'center'
+                                        }}>
+                                            {!generating && (
+                                                <img 
+                                                    style={{
+                                                        width: '90%',
+                                                        marginBottom: '30px',
+                                                    }} 
+                                                    src={previewImage} 
+                                                />
+                                            )}
+                                        </div>
+                                    </Grid>
+                                    <Grid item xs={12} md={12} lg={12}>
+                                        <Grid container>
+                                            <Grid item xs={6} md={6} lg={6}
+                                                style={{
+                                                    display: 'flex',
+                                                    justifyContent: 'flex-start',
+                                                }}
+                                            >
+                                                <CircleIconButton
+                                                    onClickHandler={() => {}}
+                                                    disabled={generating}
+                                                >
+                                                    <SendIcon />
+                                                </CircleIconButton>
+                                            </Grid>
+                                            <Grid item xs={6} md={6} lg={6}
+                                                style={{
+                                                    display: 'flex',
+                                                    justifyContent: 'flex-end',
+                                                }}
+                                            >
+                                                <CircleIconButton
+                                                    onClickHandler={downloadImage}
+                                                    disabled={generating}
+                                                >
+                                                    <SaveIcon />
+                                                </CircleIconButton>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
                             </DialogContentText>
                         </DialogContent>
                     </>
